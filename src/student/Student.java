c4b6c84c7a057ffdb8f79abfb2237995ff474b90
NicholasSullivan <sullnich@sheridancollege.ca>
2021-02-05 09:26:50 -0500

    Updated Student with getters and setters

--- src/student/Student.java
+++ src/student/Student.java
@@ -3,4 +3,20 @@ package student;
 public class Student {
     private String name;
     private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
